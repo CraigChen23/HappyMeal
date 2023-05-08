73d22a306a3bbfb25f2f66d0a45f36b7010f1dbe
Nada <nhameed30@stuy.edu>
2023-05-08 09:00:22 -0400

    created db.py wowow

--- app/__init__.py
+++ app/__init__.py
@@ -1,121 +1,14 @@
-import sqlite3
 from flask import Flask, render_template, redirect, session, request, url_for
-
+import db
 
 app = Flask(__name__)
 app.secret_key = "super"
 
-DB_FILE="users.db"
+'''DB_FILE="users.db"
 db = sqlite3.connect(DB_FILE, check_same_thread=False)
-c = db.cursor()
+c = db.cursor()'''
 
-'''METHODS============================================================='''
-
-'''create user'''
-def create_users_db():
-    DB_FILE="users.db"
-
-    db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create it
-    c = db.cursor()               #facilitate db ops -- you will use cursor to trigger db events
-    # users table
-    c.execute("CREATE TABLE IF NOT EXISTS users(user TEXT, pass TEXT)")
-
-    db.commit() #save changes
-    db.close()  #close database
-
-'''
-Adds a user into the user.db file given username and password
-*string user
-*string password
-'''
-def add_user(user, passw):
-    DB_FILE="users.db"
-
-    db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
-    c = db.cursor()               #facilitate db ops -- you will use cursor to trigger db events
-
-    # add newly registered people in
-    c.execute("INSERT INTO users (user, pass) VALUES (?,?)", (user, passw))
-
-    #prints users table
-    table = c.execute("SELECT * from users")
-    print("user table from add_user() call")
-    print(table.fetchall())
-
-    db.commit() #save changes
-    db.close()  #close database
-
-'''
-Used for user.db
-Checks if login credentials match any in the database
-'''
-def valid_login(user, passw):
-    DB_FILE="users.db"
-    db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
-    c = db.cursor()               #facilitate db ops -- you will use cursor to trigger db events
-
-    # check if username is in table
-    username = c.execute("SELECT user FROM users WHERE user = ?", (user,)).fetchone()
-
-    if username is None:
-        exists = False
-    else:
-        exists = True
-
-    # check if password is in table
-    password = c.execute("SELECT pass FROM users WHERE pass =?", (passw,)).fetchone()
-    if password is None:
-        exists = False
-
-    db.commit() #save changes
-    db.close()  #close database
-    return exists
-'''
-Used for user.db
-Checks if login credentials match any in the database
-'''
-def valid_login(user, passw):
-    DB_FILE="users.db"
-    db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
-    c = db.cursor()               #facilitate db ops -- you will use cursor to trigger db events
-
-    # check if username is in table
-    username = c.execute("SELECT user FROM users WHERE user = ?", (user,)).fetchone()
-
-    if username is None:
-        exists = False
-    else:
-        exists = True
-
-    # check if password is in table
-    password = c.execute("SELECT pass FROM users WHERE pass =?", (passw,)).fetchone()
-    if password is None:
-        exists = False
-
-    db.commit() #save changes
-    db.close()  #close database
-    return exists
-
-def user_exists(user):
-    DB_FILE="users.db"
-    db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
-    c = db.cursor()               #facilitate db ops -- you will use cursor to trigger db events
-
-    username = c.execute("SELECT user FROM users WHERE user = ?", (user,)).fetchone()
-
-    if username is None:
-        exists = False
-    else:
-        exists = True
-
-    db.commit() #save changes
-    db.close()  #close database
-    return exists
-
-create_users_db()
-
-
-'''FLASK================================================='''
+db.create_users_db()
 
 '''
 root route, renders the login page
@@ -139,7 +32,7 @@ def authenticate():
     passw = request.form['password']
     #print(db.valid_login(user, passw))
 
-    if valid_login(user, passw):
+    if db.valid_login(user, passw):
         session['username'] = user
         return render_template('home.html', msg = "successfully logged in")
     else:
@@ -157,10 +50,10 @@ def register_account():
     passw = request.form['newPass']
     #passw = request.form.get('newPass')
 
-    if user_exists(user):
+    if db.user_exists(user):
         return render_template('register.html', msg="username is in use!")
     else:
-        add_user(user, passw)
+        db.add_user(user, passw)
         return render_template('login.html', msg = "user registered!, log in with your new credentials.")
 
 @app.route("/logout", methods=['GET', 'POST'])
---
+++ app/db.py
@@ -0,0 +1,81 @@
+#database file
+
+import sqlite3
+
+'''create user'''
+def create_users_db():
+    DB_FILE="users.db"
+
+    db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create it
+    c = db.cursor()               #facilitate db ops -- you will use cursor to trigger db events
+    # users table
+    c.execute("CREATE TABLE IF NOT EXISTS users(user TEXT, pass TEXT)")
+
+    db.commit() #save changes
+    db.close()  #close database
+
+'''
+Adds a user into the user.db file given username and password
+*string user
+*string password
+'''
+def add_user(user, passw):
+    DB_FILE="users.db"
+
+    db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
+    c = db.cursor()               #facilitate db ops -- you will use cursor to trigger db events
+
+    # add newly registered people in
+    c.execute("INSERT INTO users (user, pass) VALUES (?,?)", (user, passw))
+
+    #prints users table
+    table = c.execute("SELECT * from users")
+    print("user table from add_user() call")
+    print(table.fetchall())
+
+    db.commit() #save changes
+    db.close()  #close database
+
+
+'''
+Used for user.db
+Checks if login credentials match any in the database
+'''
+def valid_login(user, passw):
+    DB_FILE="users.db"
+    db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
+    c = db.cursor()               #facilitate db ops -- you will use cursor to trigger db events
+
+    # check if username is in table
+    username = c.execute("SELECT user FROM users WHERE user = ?", (user,)).fetchone()
+
+    if username is None:
+        exists = False
+    else:
+        exists = True
+
+    # check if password is in table
+    password = c.execute("SELECT pass FROM users WHERE pass =?", (passw,)).fetchone()
+    if password is None:
+        exists = False
+
+    db.commit() #save changes
+    db.close()  #close database
+    return exists
+
+def user_exists(user):
+    DB_FILE="users.db"
+    db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
+    c = db.cursor()               #facilitate db ops -- you will use cursor to trigger db events
+
+    username = c.execute("SELECT user FROM users WHERE user = ?", (user,)).fetchone()
+
+    if username is None:
+        exists = False
+    else:
+        exists = True
+
+    db.commit() #save changes
+    db.close()  #close database
+    return exists
+
