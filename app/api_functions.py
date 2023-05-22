8e56621e56fb4b0747bf66cfd012d33d6bcae029
erica1i <eli30@stuy.edu>
2023-05-22 09:19:21 -0400

    api

--- app/api_functions.py
+++ app/api_functions.py
@@ -11,31 +11,57 @@ def key_Geoapify():
         return 'False'
 
 #gets the country's currency based off its name 
-def get_currency(country_name):
+def get_lang(country_name):
     name = country_name
   #  res = request.get(f'https://restcountries.com/v3.1/name/{name}?fullText=true')
     data = urllib.request.urlopen(f'https://restcountries.com/v3.1/name/{name}?fullText=true')
     country_json = json.load(data)
     #print(country_json)
-    currency = country_json[0]["currencies"]["CNY"]["name"]
-    return currency
-
-print(get_currency('China'))
+    currency = country_json[0]["languages"].values()
+    currency = list(currency)
+    return (currency[0])
 
 #gets the country's gini coefficient based off its name 
 def get_gini(country_name):
     name = country_name
-    url = 'https://restcountries.com/v3.1/name/{name}?fullText=true'
-    res = requests.get(url) 
-    json = res.json() 
-    gini = json["gini"][0]
-    return gini
+    data = urllib.request.urlopen(f'https://restcountries.com/v3.1/name/{name}?fullText=true')
+    country_json = json.load(data)
+    gini = country_json[0]["gini"].values()
+    gini = list(gini)
+    return (gini[0])
 
 #gets the country's capital based off its name 
 def get_capital(country_name):
     name = country_name
-    url = 'https://restcountries.com/v3.1/name/{name}?fullText=true'
-    res = requests.get(url) 
-    json = res.json() 
-    capital = json["capital"][0]
-    return capital
+    data = urllib.request.urlopen(f'https://restcountries.com/v3.1/name/{name}?fullText=true')
+    country_json = json.load(data)
+    gini = country_json[0]["capital"][0]
+    return gini
+
+#gets the country's common name based off its name 
+def get_common_name(country_name):
+    name = country_name
+    data = urllib.request.urlopen(f'https://restcountries.com/v3.1/name/{name}?fullText=true')
+    country_json = json.load(data)
+    gini = country_json[0]["name"]["common"]
+    return gini
+
+#gets the country's official name based off its name 
+def get_official_name(country_name):
+    name = country_name
+    data = urllib.request.urlopen(f'https://restcountries.com/v3.1/name/{name}?fullText=true')
+    country_json = json.load(data)
+    gini = country_json[0]["name"]["official"]
+    return gini
+
+print(get_lang('China'))
+print(get_gini('China'))
+print(get_capital('China'))
+print(get_common_name('China'))
+print(get_official_name('China'))
+
+print(get_lang('sweden'))
+print(get_gini('sweden'))
+print(get_capital('sweden'))
+print(get_common_name('sweden'))
+print(get_official_name('sweden'))
